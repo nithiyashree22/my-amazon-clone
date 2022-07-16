a958e4dea23db7e5659c6e3e41e2fbe68d3377c8
Nithiyashree <snithiyashree22@gmail.com>
2022-07-16 23:52:49 +0530

    Updates

---
+++ precache-manifest.dd44a43504ee7759f328c932f6d84ce2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ce42c234794d229aa76",
+    "url": "/my-amazon-clone/static/css/main.98833263.chunk.css"
+  },
+  {
+    "revision": "3ce42c234794d229aa76",
+    "url": "/my-amazon-clone/static/js/main.3ce42c23.chunk.js"
+  },
+  {
+    "revision": "e67e65479d4f06baac99",
+    "url": "/my-amazon-clone/static/js/1.e67e6547.chunk.js"
+  },
+  {
+    "revision": "bb61957d12d09ada3afb",
+    "url": "/my-amazon-clone/static/js/2.bb61957d.chunk.js"
+  },
+  {
+    "revision": "7841f72442a900ea0672",
+    "url": "/my-amazon-clone/static/js/runtime~main.7841f724.js"
+  },
+  {
+    "revision": "6f5d7b836c7dda4c53abcb2617ebf795",
+    "url": "/my-amazon-clone/index.html"
+  }
+];
